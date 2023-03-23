b3bd8825b57ba98f9f4fb0766840fd8769564a58
darkimpow <darkimpowell@gmail.com>
2023-03-22 20:24:44 -0400

    Api inserted for pull and placement of text entered

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,35 +1,53 @@
-import {useState,useRef} from 'react';
+import {useState, useRef} from 'react';
+import axios from "axios";
 
 const Student = () => {
-    {/*declaring the student name and address*/}
+    const [errorMessage, setErrorMessage] = useState('');
     const nameRef = useRef();
-    const addressRef =useRef();
+    const addressRef = useRef();
     const emailRef = useRef();
 
-    {/*creating a submit prevention feature for student form */}
-
-    const handleSchoolSubmit = async e => {
+    const handleSchoolSubmit = async (e) => {
         e.preventDefault();
+        setErrorMessage('');
 
-        {/* here we will console log the students name & addresses*/}
-
-        console.log(nameRef.current.value);
-        console.log(addressRef.current.value);
-        console.log(emailRef.current.value)
-
+        try {
+            const { data } = await axios.post(
+                'https://yhshmwvprwbcxpvxlihh.supabase.co/rest/v1/students',
+                {
+                    name: nameRef.current.value,
+                    address: addressRef.current.value,
+                    email: emailRef.current.value,
+                },
+                {
+                    headers: {
+                        apikey:
+                            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inloc2htd3ZwcndiY3hwdnhsaWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg1NTczMjYsImV4cCI6MTk5NDEzMzMyNn0.nMXTcW8_a0E5l5Ud0B0_q_uD6ByKonuuSl70AEPMFGU',
+                        Authorization:
+                            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inloc2htd3ZwcndiY3hwdnhsaWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg1NTczMjYsImV4cCI6MTk5NDEzMzMyNn0.nMXTcW8_a0E5l5Ud0B0_q_uD6ByKonuuSl70AEPMFGU',
+                    },
+                }
+            );
+            console.log(data);
+            nameRef.current.value = '';
+            addressRef.current.value = '';
+            emailRef.current.value = '';
+        } catch (error) {
+            setErrorMessage('Error submitting form');
+        }
     };
 
     return (
         <div>
             <form className='flex flex-col w-36 justify-center ' onSubmit={handleSchoolSubmit}>
-                <input type="text" placeholder='student name' ref={nameRef}/>
-                <input type="text" placeholder='Address' ref={addressRef}/>
-                <input type="text" placeholder='Email' ref={emailRef}/>
-                <input type="submit" value='Add to list' />
+                <input type='text' placeholder='student name' ref={nameRef} />
+                <input type='text' placeholder='Address' ref={addressRef} />
+                <input type='text' placeholder='Email' ref={emailRef} />
+                <input type='submit' value='Add to list' />
             </form>
+            {errorMessage && <div className='text-red-500'>{errorMessage}</div>}
         </div>
     );
 };
 
 export default Student;
-
